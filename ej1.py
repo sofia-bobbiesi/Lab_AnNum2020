8d765b4afe29b3e7055cbd2b3409320ddcd29457
Sofia <sofiabobbiesi@gmail.com>
2020-06-20 14:00:42 -0300

    it works

--- ej1.py
+++ ej1.py
@@ -19,18 +19,35 @@ def spline_velocidad(ts, vs):
         i_p.append(spline_c(l_p[j]))
 
     return l_p,i_p
-"""
+
 x = [0.0, 0.22, 0.85, 1.0, 1.5, 1.6, 1.99]
 y = [0.0, 0.1, -0.15, -0.03, 0.75, -0.3, 0.01]
+"""
 xgraph, ygraph = spline_velocidad(x, y)
 plt.plot(x, y, 'o', xgraph, ygraph, '-')
 plt.legend(['puntos', 'spline cubico'], loc='best')
 plt.show() """
 
 #Ejercicio 1b
+#x = vp
 
-def trapecio_adaptativo(p, v_p):
-    return None
+x0 = [0, 6, 12, 18, 24]
+y0 = [38, 41, 46, 48, 45]
+
+
+def trapecio_adaptativo(p, pv):
+    n = len(p)
+    a = p[0]
+    b = p[n-1]
+    h = (b-a)/(n-1)
+
+    pv_r = pv[1:] # right endpoints
+    pv_l = pv[:-1] # left endpoints
+    T = (h/2) * np.sum(pv_r + pv_l)
+    return T
+
+res = trapecio_adaptativo(x0, y0)
+print(res)
 
 def posicion_particula():
     return None
