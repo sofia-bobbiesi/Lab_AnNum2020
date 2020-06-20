43b4c148d4175c3928f38494ac0087d9c941aa5a
Valen Arrascaeta <valen.arrascaeta@gmail.com>
2020-06-20 13:03:05 -0300

    ej1 ready

--- spline.py
+++ spline.py
@@ -1,19 +1,26 @@
 from scipy.interpolate import interp1d
 import numpy as np
 import matplotlib.pyplot as plt
-x = [0.0, 0.22, 0.85, 1.0, 1.5, 1.6, 1.99]
-y = [0.0, 0.1, -0.15, -0.03, 0.75, -0.3, 0.01]
-xnew = []
-xnew.append(x[0])
-for j in range(len(x)-1):
-    xnew.append((x[j]+x[j+1])/2)
-    xnew.append(x[j+1])
-f = interp1d(x,y, kind='cubic')
-plt.plot(x, y, 'o', xnew, f(xnew), '-')
-plt.legend(['data', 'cubic'], loc='best')
-plt.show()
-
 
 def spline_velocidad(ts, vs):
+    prom = []
+    if len(ts)>=0:
+        prom.append(ts[0])
+
+    for i in range(len(ts)-1):
+        prom.append((ts[i]+ts[i+1])/2)
+        prom.append(ts[i+1])
     
-    return None
+    spline_cb = interp1d(ts, vs, kind='cubic')
+    interp = []
+    for j in range(len(prom)):
+        interp.append(spline_cb(prom[j]))
+
+    return prom, interp
+
+x = [0.0, 0.22, 0.85, 1.0, 1.5, 1.6, 1.99]
+y = [0.0, 0.1, -0.15, -0.03, 0.75, -0.3, 0.01]
+xgraph, ygraph = spline_velocidad(x, y)
+plt.plot(x, y, 'o', xgraph, ygraph, '-')
+plt.legend(['puntos', 'spline cubico'], loc='best')
+plt.show()
