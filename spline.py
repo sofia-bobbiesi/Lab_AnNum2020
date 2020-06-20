aee3090c8932cabd9dc0403bfd3edcd71907d247
Valen Arrascaeta <valen.arrascaeta@gmail.com>
2020-06-20 12:41:56 -0300

    fix 2

--- spline.py
+++ spline.py
@@ -4,15 +4,16 @@ import matplotlib.pyplot as plt
 x = [0.0, 0.22, 0.85, 1.0, 1.5, 1.6, 1.99]
 y = [0.0, 0.1, -0.15, -0.03, 0.75, -0.3, 0.01]
 xnew = []
-for j in range (len(x)-1):
-    xnew.append(x[j])
+xnew.append(x[0])
+for j in range(len(x)-1):
     xnew.append((x[j]+x[j+1])/2)
+    xnew.append(x[j+1])
 f = interp1d(x,y, kind='cubic')
 plt.plot(x, y, 'o', xnew, f(xnew), '-')
 plt.legend(['data', 'cubic'], loc='best')
 plt.show()
 
 
-def spline_velocidad():
+def spline_velocidad(ts, vs):
     
     return None
