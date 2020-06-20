6ce13ed5ac7dedcd6649dc51c8f911f90f402339
Valen Arrascaeta <valen.arrascaeta@gmail.com>
2020-06-20 12:25:14 -0300

    fix 1

--- spline.py
+++ spline.py
@@ -3,7 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 x = [0.0, 0.22, 0.85, 1.0, 1.5, 1.6, 1.99]
 y = [0.0, 0.1, -0.15, -0.03, 0.75, -0.3, 0.01]
-xnew = np.linspace(0, 1.99, num=40, endpoint=True)
+xnew = []
+for j in range (len(x)-1):
+    xnew.append(x[j])
+    xnew.append((x[j]+x[j+1])/2)
 f = interp1d(x,y, kind='cubic')
 plt.plot(x, y, 'o', xnew, f(xnew), '-')
 plt.legend(['data', 'cubic'], loc='best')
